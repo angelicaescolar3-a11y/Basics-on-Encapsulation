d1ab4d33e355ed72e0cb056a681331a44c4c97f8
ACT-STUDENT <ACT-STUDENT@LAB3-PC03>
2025-08-27 10:31:26 +0800

    Basics on Encapsulation

---
+++ Basics on Encapsulation/Basics on Encapsulation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics_on_Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate the bag from the fields
+            Bag bag1 = new Bag();
+
+            //Set properties using the setter method
+            bag1.Brand = "Belinda";
+            bag1.Price = 300;
+
+            //Display bag dertails using the method
+            bag1.DisplayBagDetails();
+
+            Console.ReadKey();
+        }
+
+        
+    }
+}
---
+++ Basics on Encapsulation/Basics on Encapsulation/bag.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics_on_Encapsulation
+{
+    internal class Bag
+    {
+        //NAME: ANGELICA ESCOLAR
+        //SECTION: IT301
+        //EXERCISE TITLE: Basics on Encapsulation
+
+        //declare bag fields
+        private string brand;
+        private int price;
+
+        //Set properties for the fields
+        public string Brand
+        {
+            get { return brand; }
+            set { brand = value; }
+        }
+        public int Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+        //Method to display bag details
+        public void DisplayBagDetails()
+        {
+            Console.WriteLine("Brand: " + brand);
+            Console.WriteLine("Price: " + price);
+        }
+
+    }
+}
